c0ee484c4a6278976363c594f8634cdc4dc5c727
Ke Wang <wangkekekexili@gmail.com>
2015-02-24 15:29:30 -0500

    add Article class

---
+++ main/java/wikitrust/wikipedia/Article.java
@@ -0,0 +1,13 @@
+package wikitrust.wikipedia;
+
+/**
+ * Article Objects represent Wikipedia articles
+ * @author Ke Wang
+ *
+ */
+public class Article {
+	
+	private int pageId;
+	private String pageTitle;
+	
+}
