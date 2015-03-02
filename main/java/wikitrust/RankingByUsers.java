64a8258850cf0d0367c251a1e5c58c9272180433
Ke Wang <wangkekekexili@gmail.com>
2015-03-02 11:13:13 -0500

    update rating and some results

--- main/java/wikitrust/RankingByUsers.java
+++ main/java/wikitrust/RankingByUsers.java
@@ -12,6 +12,9 @@ import java.util.Scanner;
 
 public class RankingByUsers {
 
+	public static int MINIMUM_NUMBER_OF_USERS = 200;
+	public static int MINIMUM_NUMBER_OF_REGISTERED_USERS = 15;
+	
 	class RankInfo {
 		int id;
 		String article;
@@ -48,6 +51,18 @@ public class RankingByUsers {
 			return this.numberOfUsers;
 		}
 		
+		int getNumberOfRegisteredUsers() {
+			return this.numberOfRegisteredUsers;
+		}
+		
+		double getAverageRating() {
+			return this.averageRating;
+		}
+		
+		double getAverageRatingByRegisteredUsers() {
+			return this.averageRatingByRegisteredUsers;
+		}
+		
 		double getAverageTrustworthyRating() {
 			return this.averageTrustworthyRating;
 		}
@@ -60,7 +75,7 @@ public class RankingByUsers {
 		try (Scanner sc = new Scanner(f)) {
 			while (sc.hasNextLine()) {
 				RankInfo temp = new RankInfo(sc.nextLine());
-				if (temp.getNumberOfUsers() >= 50) {
+				if (temp.getNumberOfUsers() >= MINIMUM_NUMBER_OF_USERS) {
 					list.add(temp);
 				}
 			}
@@ -68,16 +83,23 @@ public class RankingByUsers {
 			System.err.println(f);
 			System.exit(1);
 		}
+		
+		// order by average trustworthy rating 
 		Collections.sort(list, (e1, e2)->{
 			if (((RankInfo)e1).getAverageTrustworthyRating() > ((RankInfo)e2).getAverageTrustworthyRating()) {
 				return -1;
 			} else if (((RankInfo)e1).getAverageTrustworthyRating() < ((RankInfo)e2).getAverageTrustworthyRating()) {
 				return 1;
 			} else {
-				return 0;
+				if (e1.getNumberOfUsers() > e2.getNumberOfUsers()) {
+					return -1;
+				} else if (e1.getNumberOfUsers() < e2.getNumberOfUsers()) {
+					return 1;
+				} else {
+					return 0;
+				}
 			}
 		});
-
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter("share/data/ratingOrderByAverageTrustworthyRating.tsv"))) {
 			for (RankInfo r : list) {
 				bw.write(r.getId()+"\t"
@@ -90,6 +112,98 @@ public class RankingByUsers {
 			System.exit(1);
 		}
 		
+		// order by average rating
+		Collections.sort(list, (e1, e2)->{
+			if (e1.getAverageRating() > e2.getAverageRating()) {
+				return -1;
+			} else if (e1.getAverageRating() < e2.getAverageRating()) {
+				return 1;
+			} else {
+				if (e1.getNumberOfUsers() > e2.getNumberOfUsers()) {
+					return -1;
+				} else if (e1.getNumberOfUsers() < e2.getNumberOfUsers()) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+		});
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("share/data/ratingOrderByAverageRating.tsv"))) {
+			for (RankInfo r : list) {
+				bw.write(r.getId()+"\t"
+						+r.getArticle()+"\t"
+						+r.getNumberOfUsers()+"\t"
+						+r.getAverageRating()+"\n");		
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(1);
+		}
+		
+		// consider articles with registered users more than default
+		List<RankInfo> newList = new ArrayList<RankInfo>();
+		for (RankInfo r : list) {
+			if (r.getNumberOfRegisteredUsers() >= MINIMUM_NUMBER_OF_REGISTERED_USERS) {
+				newList.add(r);
+			}
+		}
+		list = newList;
+		
+		// order by number of registered users, average rating by registered users
+		Collections.sort(list, (e1,e2)->{
+			if (e1.getNumberOfRegisteredUsers() > e2.getNumberOfRegisteredUsers()) {
+				return -1;
+			} else if (e1.getNumberOfRegisteredUsers() < e2.getNumberOfRegisteredUsers()) {
+				return 1;
+			} else {
+				if (e1.getAverageRatingByRegisteredUsers() < e2.getAverageRatingByRegisteredUsers()) {
+					return -1;
+				} else if (e1.getAverageRatingByRegisteredUsers() > e2.getAverageRatingByRegisteredUsers()) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+		});
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("share/data/ratingOrderByNumberOfRegisteredUsers.tsv"))) {
+			for (RankInfo r : list) {
+				bw.write(r.getId()+"\t"
+						+r.getArticle()+"\t"
+						+r.getNumberOfRegisteredUsers()+"\t"
+						+r.getAverageRatingByRegisteredUsers()+"\n");		
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(1);
+		}
+		
+		// order by rating by registered users, number of registered users
+		Collections.sort(list, (e1,e2)->{
+			if (e1.getAverageRatingByRegisteredUsers() > e2.getAverageRatingByRegisteredUsers()) {
+				return -1;
+			} else if (e1.getAverageRatingByRegisteredUsers() < e2.getAverageRatingByRegisteredUsers()) {
+				return 1;
+			} else {
+				if (e1.getNumberOfRegisteredUsers() < e2.getNumberOfRegisteredUsers()) {
+					return -1;
+				} else if (e1.getNumberOfRegisteredUsers() > e2.getNumberOfRegisteredUsers()) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+		});
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("share/data/ratingOrderByAverageRatingByRegisteredUsers.tsv"))) {
+			for (RankInfo r : list) {
+				bw.write(r.getId()+"\t"
+						+r.getArticle()+"\t"
+						+r.getNumberOfRegisteredUsers()+"\t"
+						+r.getAverageRatingByRegisteredUsers()+"\n");		
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(1);
+		}
 	}
 
 	public static void main(String[] args) {
